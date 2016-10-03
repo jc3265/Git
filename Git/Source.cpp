6ad36ec34ea077248fa7661d5cbaa87f50eeee13
jc3265 <jc3265@FLDVP-CTXCEC01>
2016-10-02 20:41:28 -0400

    Added An extra Line, Added a header on the file

--- Git/Source.cpp
+++ Git/Source.cpp
@@ -1,5 +1,13 @@
+/*
+* File: Source.cpp
+* Project: Git
+*  Created on: Oct 02, 2016
+*      Author: Juan Caldas
+*/
 
 #include <iostream>
+
 int main() {
 	std::cout << "Hello World" << std::endl;
+	std::cout << "Second Line on my Hellow World" << std::endl;
 }
